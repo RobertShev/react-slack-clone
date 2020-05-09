87f86d7797e4a71c287e9bd4f42a36dc649a85dd
robertshev <roberto.shev@gmail.com>
2020-05-09 13:56:11 +0300

    footer styling finished

--- slack/src/component/Footer.js
+++ slack/src/component/Footer.js
@@ -14,37 +14,14 @@ function Footer() {
   return (
     <Container className="footer" fluid={true}>
       <Row>
-        <Col>
-          <svg
-            width="54"
-            height="54"
-            class="c-nav--footer__svgicon c-slackhash"
-            viewBox="0 0 54 54"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <g fill="none" fill-rule="evenodd">
-              <path
-                d="M19.712.133a5.381 5.381 0 0 0-5.376 5.387 5.381 5.381 0 0 0 5.376 5.386h5.376V5.52A5.381 5.381 0 0 0 19.712.133m0 14.365H5.376A5.381 5.381 0 0 0 0 19.884a5.381 5.381 0 0 0 5.376 5.387h14.336a5.381 5.381 0 0 0 5.376-5.387 5.381 5.381 0 0 0-5.376-5.386"
-                fill="#36C5F0"
-              ></path>
-              <path
-                d="M53.76 19.884a5.381 5.381 0 0 0-5.376-5.386 5.381 5.381 0 0 0-5.376 5.386v5.387h5.376a5.381 5.381 0 0 0 5.376-5.387m-14.336 0V5.52A5.381 5.381 0 0 0 34.048.133a5.381 5.381 0 0 0-5.376 5.387v14.364a5.381 5.381 0 0 0 5.376 5.387 5.381 5.381 0 0 0 5.376-5.387"
-                fill="#2EB67D"
-              ></path>
-              <path
-                d="M34.048 54a5.381 5.381 0 0 0 5.376-5.387 5.381 5.381 0 0 0-5.376-5.386h-5.376v5.386A5.381 5.381 0 0 0 34.048 54m0-14.365h14.336a5.381 5.381 0 0 0 5.376-5.386 5.381 5.381 0 0 0-5.376-5.387H34.048a5.381 5.381 0 0 0-5.376 5.387 5.381 5.381 0 0 0 5.376 5.386"
-                fill="#ECB22E"
-              ></path>
-              <path
-                d="M0 34.249a5.381 5.381 0 0 0 5.376 5.386 5.381 5.381 0 0 0 5.376-5.386v-5.387H5.376A5.381 5.381 0 0 0 0 34.25m14.336-.001v14.364A5.381 5.381 0 0 0 19.712 54a5.381 5.381 0 0 0 5.376-5.387V34.25a5.381 5.381 0 0 0-5.376-5.387 5.381 5.381 0 0 0-5.376 5.387"
-                fill="#E01E5A"
-              ></path>
-            </g>
-          </svg>
+        <Col className="seconday-logo">
+          <img src="images/small-logo.svg" alt="Slack logo"></img>
         </Col>
         <Col>
-          <p>Product</p>
           <ul className="footer-list">
+            <li>
+              <p>Product</p>
+            </li>
             <li>Features</li>
             <li>Enterprise</li>
             <li>Security</li>
@@ -54,8 +31,10 @@ function Footer() {
           </ul>
         </Col>
         <Col>
-          <p>SLACK FOR TEAMS</p>
           <ul className="footer-list">
+            <li>
+              <p>SLACK FOR TEAMS</p>
+            </li>
             <li>Engineering</li>
             <li>Financial Services</li>
             <li>Sales</li>
@@ -68,8 +47,10 @@ function Footer() {
           </ul>
         </Col>
         <Col>
-          <p>RESOURCES</p>
           <ul className="footer-list">
+            <li>
+              <p>RESOURCES</p>
+            </li>
             <li>Slack Tips</li>
             <li>Blog</li>
             <li>Events</li>
@@ -82,8 +63,10 @@ function Footer() {
           </ul>
         </Col>
         <Col>
-          <p>COMPANY</p>
           <ul className="footer-list">
+            <li>
+              <p>COMPANY</p>
+            </li>
             <li>About Us</li>
             <li>Leadership</li>
             <li>News</li>
@@ -93,9 +76,9 @@ function Footer() {
           </ul>
         </Col>
       </Row>
-      <Row>
-        <Col>
-          <ul className="informational-list">
+      <Row className="footer-secondary">
+        <Col className="informational-list">
+          <ul>
             <li>
               <a>Status</a>
             </li>
@@ -119,9 +102,11 @@ function Footer() {
         </Col>
       </Row>
       <Row>
-        <Col>
-          © Copyright 2020 Slack Technologies, Inc. All rights reserved. Various
-          trademarks held by their respective owners.
+        <Col className="footer-copyright">
+          <span>
+            &copy; Copyright 2020 Slack Technologies, Inc. All rights reserved.
+            Various trademarks held by their respective owners.
+          </span>
         </Col>
       </Row>
     </Container>
