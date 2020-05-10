ad2a344d417543b1bc45ebcb0c64796d8e133af5
robertshev <roberto.shev@gmail.com>
2020-05-11 01:28:07 +0300

    Updates

---
+++ precache-manifest.14425272ef4d6b285a4b68b044e1f356.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40c011efbc5231aba1eeea3334593a80",
+    "url": "/react-slack-clone/index.html"
+  },
+  {
+    "revision": "39b80a4d37845068f92b",
+    "url": "/react-slack-clone/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "5a1cf8de757c2d88362e",
+    "url": "/react-slack-clone/static/css/main.4c73f5e3.chunk.css"
+  },
+  {
+    "revision": "39b80a4d37845068f92b",
+    "url": "/react-slack-clone/static/js/2.9fba6bc7.chunk.js"
+  },
+  {
+    "revision": "ed2f89ced4f72ba02fe3ffe8bbf3bb5f",
+    "url": "/react-slack-clone/static/js/2.9fba6bc7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a1cf8de757c2d88362e",
+    "url": "/react-slack-clone/static/js/main.62e108bc.chunk.js"
+  },
+  {
+    "revision": "eef0ff039266d03cf5ab",
+    "url": "/react-slack-clone/static/js/runtime-main.e866cd20.js"
+  }
+]);
